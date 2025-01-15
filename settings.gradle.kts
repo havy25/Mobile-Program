3b2e17d40eeda0f5227b917aa544dae613e0a04c
havy25 <havy02052005@gmail.com>
2025-01-15 11:59:28 +0700

    lab3_Affirmations App

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+<<<<<<< HEAD
+rootProject.name = "Dice Roller"
+=======
+rootProject.name = "Affirmations App"
+>>>>>>> 16a741c (lab3_Affirmations App)
+include(":app")
+ 
